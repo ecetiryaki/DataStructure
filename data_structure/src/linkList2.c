ca0ded7a72e77373fcd2c33e337c7829157e0464
Ece  Tiryaki <ecetiryaki@1b2a.wls.metu.edu.tr>
2019-01-16 11:18:55 +0300

    blabla

--- data_structure/src/linkList2.c
+++ data_structure/src/linkList2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//stack
+
 struct node{
   int data;
   char key;
@@ -11,47 +11,58 @@ struct node *head=NULL;
 struct node *tail=NULL;
 struct node *current=NULL;
 
-void insertHead(int data, char key){//insert to the begining
-  struct node *link= (struct node*) malloc(sizeof(struct node));
-  link->data=data;
-  link->key=key;
-  link->next= head;
-  head=link;
+ void insert(int data, char key){
+   struct node *link=(struct node*) malloc(sizeof(struct node));
+   link->data=data;
+   link->key=key;
+   link->prev=current->prev;
+   link->next=current;
+   current=link;
+ }
+
+void delete(){
+  struct node *temp=current;
+  current= current->next;
+  current->prev=temp->prev;
 }
 
-void insertTail(int data, char key){//insert to the end
-  struct node *link= (struct node*) malloc(sizeof(struct node));
-  link->data=data;
-  link->key=key;
-  link->prev= tail;
-  tail=link;
+void moveRight(){
+  struct node *temp=current;
+  current=current->next;
+  current->prev= temp;
+}
+void moveLeft(){
+  struct node *temp=current;
+  current=current->prev;
+  current->next= temp;
 }
 
-struct node* delete(){//delete 1st item
-  struct node *tempLink=head;
-  head= head->next;
-  return tempLink;
-}
 void printList()
 {
-  struct node *ptr=head;
+  struct node *ptr=current;
   printf("\n[\n");
-  while (ptr!=NULL)
+  while(ptr->prev!=NULL)
   {
-      printf("%d", ptr->data);
-      printf("%c\n", ptr->key);
-      ptr=ptr->next;
+    ptr = ptr->prev;
+  }
+  while (ptr!=NULL && ptr->prev==NULL)
+  {
+    printf("%d", ptr->data);
+    printf("%c\n", ptr->key);
+    ptr=ptr->next;
   }
   printf("]\n");
 }
 
 int main(void){
-  insertHead(1,'a');
-  insertTail(2,'b');
-  insertHead(3,'c');
-  insertHead(4,'d');
-  insertHead(5,'e');
+  insert(1,'a');
+  insert(3,'c');
+  insert(4,'d');
+  moveRight();
+  //moveLeft();
+  insert(7,'k');
+  insert(5,'e');
   printList();
-  delete();
-  printList();
+  //delete();
+  //printList();
 }
